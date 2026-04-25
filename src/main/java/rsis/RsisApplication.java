aa58071be72c317c5a6b594f9284296380886105
hfzfjr <hafizfajar0101@gmail.com>
2026-04-25 15:02:09 +0700

    first commit

---
+++ src/main/java/rsis/RsisApplication.java
@@ -0,0 +1,13 @@
+package rsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsisApplication.class, args);
+	}
+
+}

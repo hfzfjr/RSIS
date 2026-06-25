041fd97637eec32b3d9dbe3aed0cc7619f20178c
hfzfjr <hafizfajar0101@gmail.com>
2026-06-25 22:03:19 +0700

    update annnotation kuning

--- src/main/java/rsis/config/WebConfig.java
+++ src/main/java/rsis/config/WebConfig.java
@@ -2,6 +2,7 @@ package rsis.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${upload.path:uploads}")
     private String uploadPath;
 
+    /**
+     * Configure resource handlers for serving uploaded files.
+     * This method is called automatically by Spring MVC during application startup.
+     * 
+     * @param registry the ResourceHandlerRegistry to configure
+     */
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve uploaded files
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        // Serve uploaded files from the configured upload path
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
     }
